0a312f0defe1fd009ed35bc02fac2a1d21df9061
viniljose <vinilpj@gmail.com>
2021-09-30 09:47:12 +0530

    Create tuples.py

---
+++ tuples.py
@@ -0,0 +1,12 @@
+vowelsL = ['a','e','i','o','u']
+print(type(vowelsL))
+vowelsT = ('a','e','i','o','u')
+print(type(vowelsT))
+vowelsL[2] = 'I'
+print(vowelsL)
+#vowelsT[2] = 'I'
+print(vowelsT)
+t = ('python')
+print(type(t))
+t1 = ('python',)
+print(type(t1))
